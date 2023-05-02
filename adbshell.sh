0b0ab1457adeb0edd082f776ae80544fd4c3cb66
Sara Mentr <saramentr@gmail.com>
2023-05-02 10:48:52 +0300

    Update adbshell.sh

--- adbshell.sh
+++ adbshell.sh
@@ -3,6 +3,7 @@ cd /sys/devices/system/cpu
 echo performance | tee cpu*/cpufreq/scaling_governor
 cd /data/local/tmp
 ./afl-fuzz -O -G 256 -i in -o out ./fuzz
+#./afl-fuzz -O -G 256 -t 600000 -i in -o out ./fuzz
 #AFL_DEBUG=1 AFL_QEMU_DEBUG_MAPS=1 ./afl-fuzz -O -G 256 -i in -o out ./fuzz
 #AFL_DEBUG_CHILD=1 AFL_MAP_SIZE=10000000 AFL_DEBUG=1 AFL_QEMU_DEBUG_MAPS=1 ./afl-fuzz -O -G 256 -i in -o out ./fuzz
 #for f in out/default/queue/id*;do xxd $f;done
