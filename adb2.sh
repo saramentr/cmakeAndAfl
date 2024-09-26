36331073fac417fdbf085b752c468da3ca2bb385
Sara Mentr <saramentr@gmail.com>
2024-09-26 17:49:36 +0300

    Update adb2.sh

--- adb2.sh
+++ adb2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+adb wait-for-device
 adb shell "su 0 whoami; cd /sys/devices/system/cpu;su 0 echo performance | tee cpu*/cpufreq/scaling_governor"
 adb shell "su 0 whoami;cd /data/local/tmp/;su 0 ./afl-fuzz -O -G 256 -i in -o out ./fuzz"
