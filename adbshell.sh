20f5556ea43686fb779fe7d690d995fe3331c3ef
Sara Mentr <saramentr@gmail.com>
2024-09-26 18:51:05 +0300

    Update adbshell.sh

--- adbshell.sh
+++ adbshell.sh
@@ -16,4 +16,4 @@ adb reboot
 adb wait-for-device
 adb shell "su 0 whoami; cd /sys/devices/system/cpu;su 0 echo performance | tee cpu*/cpufreq/scaling_governor"
 adb shell "su 0 whoami;cd /data/local/tmp/;su 0 timeout 10m ./afl-fuzz -O -G 256 -i in -o out ./fuzz"
-adb shell "su 0 cat /data/local/tmp/out3/default/fuzzer_stats"
+adb shell "su 0 cat /data/local/tmp/out/default/fuzzer_stats"
