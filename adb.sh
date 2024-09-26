133cd2ebebd79986217ca50973a8992097d9237d
Sara Mentr <saramentr@gmail.com>
2024-09-26 17:49:22 +0300

    Update adb.sh

--- adb.sh
+++ adb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+adb wait-for-device
 cd /opt/AFLplusplus-4.06c/build/&& adb push afl-fuzz afl-frida-trace.so /data/local/tmp
 cd /opt/android-fuzzing/native/build&&adb push fuzz ../afl.js ../lib/libblogfuzz.so /data/local/tmp
 #cd /opt/android-fuzzing/wlinked_jni/build/&&adb push fuzz ../afl.js ../lib/libblogfuzz.so ../lib/libjenv.so  /data/local/tmp
