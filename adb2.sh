c2b58edaa71a31919f505e5786179b410c2b33f4
Sara Mentr <saramentr@gmail.com>
2024-09-26 17:38:37 +0300

    Update adb2.sh

--- adb2.sh
+++ adb2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-adb shell "su 0 "whoami; cd /sys/devices/system/cpu&&echo performance | tee cpu*/cpufreq/scaling_governor&&whoami""
-adb shell "su 0 "whoami;cd /data/local/tmp&& AFL_DEBUG=1 ./afl-fuzz -O -G 256 -i in -o out ./fuzz&& whoami"
+adb shell "su 0 whoami; cd /sys/devices/system/cpu;su 0 echo performance | tee cpu*/cpufreq/scaling_governor"
+adb shell "su 0 whoami;cd /data/local/tmp/;su 0 ./afl-fuzz -O -G 256 -i in -o out ./fuzz"
