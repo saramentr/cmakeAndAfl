d7d35292c437020c59fb182c67062a9edc1d9380
Sara Mentr <saramentr@gmail.com>
2024-09-26 17:49:04 +0300

    Update adbshell.sh

--- adbshell.sh
+++ adbshell.sh
@@ -1,9 +1,18 @@
-su
-cd /sys/devices/system/cpu
-echo performance | tee cpu*/cpufreq/scaling_governor
-cd /data/local/tmp
-./afl-fuzz -O -G 256 -i in -o out ./fuzz
-#./afl-fuzz -O -G 256 -t 600000 -i in -o out ./fuzz
-#AFL_DEBUG=1 AFL_QEMU_DEBUG_MAPS=1 ./afl-fuzz -O -G 256 -i in -o out ./fuzz
-#AFL_DEBUG_CHILD=1 AFL_MAP_SIZE=10000000 AFL_DEBUG=1 AFL_QEMU_DEBUG_MAPS=1 ./afl-fuzz -O -G 256 -i in -o out ./fuzz
-#for f in out/default/queue/id*;do xxd $f;done
+#!/bin/bash
+adb wait-for-device
+cd /opt/AFLplusplus-4.06c/build/&& adb push afl-fuzz afl-frida-trace.so /data/local/tmp
+cd /opt/android-fuzzing/native/build&&adb push fuzz ../afl.js ../lib/libblogfuzz.so /data/local/tmp
+#cd /opt/android-fuzzing/wlinked_jni/build/&&adb push fuzz ../afl.js ../lib/libblogfuzz.so ../lib/libjenv.so  /data/local/tmp
+#cd /opt/android-fuzzing/slinked_jni/build/&&adb push fuzz ../afl.js ../lib/libblogfuzz.so ../lib/libjenv.so ../mock.dex ../../apk/qb.blogfuzz.apk /data/local/tmp
+adb shell mkdir /data/local/tmp/in
+adb shell mkdir /data/local/tmp/out
+adb shell dd if=/dev/urandom of=/data/local/tmp/in/sample.bin bs=1 count=16
+
+adb root
+adb disable verity
+adb remount
+adb reboot
+
+adb wait-for-device
+adb shell "su 0 whoami; cd /sys/devices/system/cpu;su 0 echo performance | tee cpu*/cpufreq/scaling_governor"
+adb shell "su 0 whoami;cd /data/local/tmp/;su 0 ./afl-fuzz -O -G 256 -i in -o out ./fuzz"
